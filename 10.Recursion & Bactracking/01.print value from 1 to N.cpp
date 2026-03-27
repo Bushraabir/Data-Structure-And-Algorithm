fbeeec7713a34f043fbee4ff3622aeeb42afc73b
Bushraabir <186773951+Bushraabir@users.noreply.github.com>
2026-03-27 10:45:29 +0600

    updated

--- 10.Recursion & Bactracking/01.print value from 1 to N.cpp
+++ 10.Recursion & Bactracking/01.print value from 1 to N.cpp
@@ -24,3 +24,14 @@ int main() {
 
     return 0;
 }
+
+
+// Time Complexity: O(n) — Each number from 1 to N is printed once, and the function is called recursively N times until it reaches the base case (n == 0).
+// Space Complexity: O(n) — The maximum depth of the recursion is N, which means there will be N function calls on the call stack at the worst case before it starts returning back up.
+
+
+// we use recursion in the following way:
+// 1. We check if n is 0, which is our base case. If n is 0, we return from the function, which stops the recursion.
+// 2. If n is not 0, we make a recursive call to printUptoN with n-1. This means we are asking the function to print numbers from 1 to n-1 first.
+// 3. After the recursive call returns (which means it has printed numbers from 1 to n-1), we print the current value of n. 
+// This way, we ensure that numbers are printed in the correct order from 1 to N.      
