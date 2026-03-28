a0284823c631a6d31014d7a88f8ef454219b57f7
Bushraabir <186773951+Bushraabir@users.noreply.github.com>
2026-03-28 09:53:54 +0600

    updated

--- 10.Recursion & Bactracking/07.Fibbonacci Numbers.cpp
+++ 10.Recursion & Bactracking/07.Fibbonacci Numbers.cpp
@@ -17,9 +17,10 @@ using namespace std;
 int fib(int n) {
     // Base cases
     if (n == 0) return 0;
-    if (n == 1) return 1;
+    if (n == 1) return 1; // as the 1st Fibonacci number is 1
 
-    // Recursive case: F(n) = F(n-1) + F(n-2)
+    // a = F(n - 1) and b = F(n - 2)
+    //in short we can write it as return fib(n - 1) + fib(n - 2);
     return fib(n - 1) + fib(n - 2);
 }
 
